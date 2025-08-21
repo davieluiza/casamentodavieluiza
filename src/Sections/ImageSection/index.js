e50b9502ce8964f44bd401ae1955b8cf5eb9274d
João Victor Morais <45773455+jovicpdm@users.noreply.github.com>
2025-08-21 09:28:22 -0300

    atualização da foto

--- src/Sections/ImageSection/index.js
+++ src/Sections/ImageSection/index.js
@@ -2,15 +2,13 @@ import React from "react";
 import "./styles.css";
 
 export default function ImageSection() {
-  return (
-    <div className="image-background">
-      <div className="overlay">
-        <p className="image-text">
-          “Melhor é serem dois do que um, porque têm melhor paga do seu
-          trabalho. Porque se caírem, um levanta o companheiro.”
-        </p>
-        <p className="image-text">Eclesiastes 3:9</p>
-      </div>
-    </div>
-  );
+    return (
+        <div className="image-background">
+            <p className="image-text">
+                “Melhor é serem dois do que um, porque têm melhor paga do seu
+                trabalho. Porque se caírem, um levanta o companheiro.”
+            </p>
+            <p className="image-text">Eclesiastes 3:9</p>
+        </div>
+    );
 }
